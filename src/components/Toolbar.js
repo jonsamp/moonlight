de43d45ae113bf61cb9cb57635dc0d94efada6ba
gvan12 <gvan09@gmail.com>
2017-04-23 16:35:32 -0500

    configure initial routes and rough out listing component. include belle.

---
+++ src/components/Toolbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import belle from 'belle';
+
+const Card = belle.Card;
+
+export default class Listings extends React.Component {
+
+  render() {
+    return (
+      <Card>
+        <h2>{'Toolbar'}</h2>
+      </Card>
+    );
+  }
+}
