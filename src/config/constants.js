542a4fc59f9316ede0bdb1c883bbc618a5ce3b47
Jon Samp <sampjon@gmail.com>
2017-04-22 23:41:19 -0400

    adds firebase keys

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 import firebase from 'firebase'
 
 const config = {
-  apiKey: "AIzaSyDHL6JFTyBcaV60WpE4yXfeO0aZbzA9Xbk",
-  authDomain: "practice-auth.firebaseapp.com",
-  databaseURL: "https://practice-auth.firebaseio.com",
+  apiKey: "AIzaSyB1kWy4wEzBFBx67yed90PalWGOMOopj7w",
+  authDomain: "moonlight-dpc.firebaseapp.com",
+  databaseURL: "https://moonlight-dpc.firebaseio.com/",
 }
 
 firebase.initializeApp(config)
