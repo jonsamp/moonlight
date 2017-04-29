1aa737a30b6c73e8b7211ab65370de794e863e6f
Jon Samp <sampjon@gmail.com>
2017-04-29 12:47:45 -0400

    minor styles to the profile avatar

--- src/components/protected/Profile.js
+++ src/components/protected/Profile.js
@@ -22,12 +22,12 @@ class Profile extends React.Component {
 
   render() {
     return (
-      <div>
+      <section className="profile">
         {this.state.user.info && <img src={this.state.user.info.avatarUrl} alt={`${this.state.user.info.displayName}'s avatar`} />}
         {this.state.user.info && <h1> {this.state.user.info.displayName} </h1>}
         <ImageUploader userId={this.props.user.uid} />
         {this.state.user.info && <Button onClick={() => deleteUser(this.state.user.info.uid, Object.values(this.state.user.posts))} style={{backgroundColor: '#A91912', color: 'white'}}>Delete Account and Posts</Button>}
-      </div>);
+      </section>);
   }
 }
 
